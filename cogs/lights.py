736db1552fa3055b7dd99e80e28748a5d10cdb65
Matt C <mdc028@bucknell.edu>
2022-02-06 17:12:17 -0500

    trying to implement hex to hsbk (thanks @axtlos :))) )

--- cogs/lights.py
+++ cogs/lights.py
@@ -18,6 +18,52 @@ class IOT(commands.Cog):
         self.bot = bot
         self.light = Light(LIFX_MAC, LIFX_IP)
 
+    def hex_to_hsbk(hex):
+        rgb=tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+        r=rgb[0]/255
+        g=rgb[1]/255
+        b=rgb[2]/255
+        # R, G, B values are divided by 255
+        # to change the range from 0..255 to 0..1:
+        r, g, b = r / 255.0, g / 255.0, b / 255.0
+
+        # h, s, v = hue, saturation, value
+        cmax = max(r, g, b)    # maximum of r, g, b
+        cmin = min(r, g, b)    # minimum of r, g, b
+        diff = cmax-cmin       # diff of cmax and cmin.
+
+        # if cmax and cmax are equal then h = 0
+        if cmax == cmin:
+            h = 0
+
+        # if cmax equal r then compute h
+        elif cmax == r:
+            h = (60 * ((g - b) / diff) + 360) % 360
+
+        # if cmax equal g then compute h
+        elif cmax == g:
+            h = (60 * ((b - r) / diff) + 120) % 360
+
+        # if cmax equal b then compute h
+        elif cmax == b:
+            h = (60 * ((r - g) / diff) + 240) % 360
+
+        # if cmax equal zero
+        if cmax == 0:
+            s = 0
+        else:
+            s = (diff / cmax) * 100
+
+        # compute v
+        v = cmax * 100
+
+        hue_lifx=int(round(0x10000 * h) / 360) % 0x10000
+        saturation_lifx=int(round(0xFFFF * s/100))
+        brightness=65535 # could be specified by the user with an extra arg
+        kelvin=4000 # could be specified by the user with an extra arg
+
+        return [hue_lifx, saturation_lifx, brightness, kelvin]
+
     @commands.command()
     async def lifx(self, ctx, *, cmd=""):
         """Set Matt's light to a color. (Run w/o arguements for help info)"""
@@ -30,7 +76,7 @@ class IOT(commands.Cog):
             "blue": BLUE,
             "purple": PURPLE,
             "pink": PINK,
-            "crystal": [50972, 65535, 65535, 4000],
+            "crystal": [50929, 65535, 65535, 4000],
         }
 
         if cmd == "":
@@ -40,6 +86,8 @@ class IOT(commands.Cog):
             await ctx.send(i)
             await ctx.send("You can also say `random`, `sample`, `on`, or `off`")
             return
+        elif "#" in cmd:
+            cmd = self.hex_to_hsbk(cmd.replace("#",""))
 
         if not os.path.exists(".lifx_disabled"):
             ran = False
@@ -55,7 +103,10 @@ class IOT(commands.Cog):
                         c = random.choice(list(colors.values()))
                         self.light.set_color(c)
                     elif cmd == "random":
-                        c = [randint(0,65535), randint(0,65535), randint(0,65535), randint(0,65535),]
+                        c = [randint(0,65535), randint(0,65535), randint(0,65535), randint(0,65535)]
+                        self.light.set_color(c)
+                    else:
+                        self.light.set_color(cmd)
                     ran = True
                 except Exception as e:
                     syslog.log("IOT", "LIFX error `" + str(e) + "`")
