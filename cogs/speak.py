a4748756c1ff50fd3dad14be54afeff7a3a5c67d
Matt C <matt@tar.black>
2022-02-10 21:35:38 -0500

    hngnhgnn guild name

--- cogs/speak.py
+++ cogs/speak.py
@@ -153,7 +153,7 @@ class Speak(commands.Cog):
                 if len(vc.members) != 0:
                     if random.randint(1, 10) == 5:
                         await self.speakInChannel(
-                            None, random.choice(IMAGE_RESPONSES), chan=vc, stealth=True
+                            None, "Hi folks of " + str(guild.name) + random.choice(IMAGE_RESPONSES), chan=vc, stealth=True
                         )
             for chan in guild.text_channels:
                 if chan.can_send():
