253278ec424d8aad666bacd29c897e6e711d7fb9
Matt C <matt@tar.black>
2022-02-12 22:49:34 -0500

    cache the funny random cat pics because i promie it's a good use of disks space :)

--- cogs/cats.py
+++ cogs/cats.py
@@ -1,15 +1,15 @@
-import os, re, random
+# Stdlib
+import os, re, random, shutil
 from random import randint
+import urllib.parse
+import asyncio
 
+# Pip
 import discord
 from discord.ext import commands
-
-import asyncio
 import requests
 
-import urllib.parse
-
-
+# Me
 from util_functions import *
 from server_config import serverconfig
 
@@ -19,13 +19,16 @@ class Cats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def save_cat(self, fn):
+        shutil.move(fn, PASTE_BASE + "/" + fn)
+
     @commands.command()
     async def catpic(self, ctx):
         r = requests.get("https://cataas.com/cat", allow_redirects=True)
         name = "".join(random.sample(string.ascii_lowercase+string.digits, 5)) + ".jpeg"
         open(name, "wb").write(r.content)
         await ctx.send(file=discord.File(name))
-        os.remove(name)
+        self.save_cat(name)
 
     @commands.command()
     async def catgif(self, ctx):
@@ -33,7 +36,7 @@ class Cats(commands.Cog):
         name = "".join(random.sample(string.ascii_lowercase+string.digits, 5)) + ".gif"
         open(name, "wb").write(r.content)
         await ctx.send(file=discord.File(name))
-        os.remove(name)
+        self.save_cat(name)
 
     @commands.command()
     async def catsays(self, ctx, *, msg):
@@ -41,7 +44,7 @@ class Cats(commands.Cog):
         name = "".join(random.sample(string.ascii_lowercase+string.digits, 5)) + ".jpeg"
         open(name, "wb").write(r.content)
         await ctx.send(file=discord.File(name))
-        os.remove(name)
+        self.save_cat(name)
 
 def setup(bot):
     bot.add_cog(Cats(bot))
