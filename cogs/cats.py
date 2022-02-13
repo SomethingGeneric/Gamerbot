03626c35100f05c9485549f0466b844e9b255cd2
Matt C <matt@tar.black>
2022-02-12 22:42:16 -0500

    cat-ption

--- cogs/cats.py
+++ cogs/cats.py
@@ -5,11 +5,13 @@ import discord
 from discord.ext import commands
 
 import asyncio
+import requests
+
+import urllib.parse
+
 
 from util_functions import *
 from server_config import serverconfig
-
-import requests
 
 class Cats(commands.Cog):
     """Just cat Things"""
@@ -29,6 +31,14 @@ class Cats(commands.Cog):
     async def catgif(self, ctx):
         r = requests.get("https://cataas.com/cat/gif", allow_redirects=True)
         name = "".join(random.sample(string.ascii_lowercase+string.digits, 5)) + ".gif"
+        open(name, "wb").write(r.content)
+        await ctx.send(file=discord.File(name))
+        os.remove(name)
+
+    @commands.command()
+    async def catsays(self, ctx, *, msg):
+        r = requests.get("https://cataas.com/cat" + urllib.parse.quote(msg.encode("utf-8")), allow_redirects=True)
+        name = "".join(random.sample(string.ascii_lowercase+string.digits, 5)) + ".jpeg"
         open(name, "wb").write(r.content)
         await ctx.send(file=discord.File(name))
         os.remove(name)
