e9d16f68453f0e427669eea231e2cb50e29ca26c
Matt C <matt@tar.black>
2022-05-10 16:59:06 -0400

    minor fixes

--- cogs/admin.py
+++ cogs/admin.py
@@ -21,37 +21,38 @@ class Admin(commands.Cog):
     async def on_message(self, message):
         if not isinstance(message.channel, discord.channel.DMChannel):
             for guild in self.bot.guilds:
-                found = False
-                for role in guild.roles:
-                    if role.name == "gb_mod":
-                        found = True
-                        break
-                if not found:
-                    if not os.path.exists(".setupdone_" + str(message.guild.id)):
+                if not os.path.exists(".setupdone_" + str(message.guild.id)):
+                    found_role = False
+
+                    for role in message.guild.roles:
+                        if role.name == "gb_mod":
+                            found_role = True
+
+                    if not found_role:
                         modr = await guild.create_role(
                             name="gb_mod",
                             reason="Role used to designate who can use our admin commands.",
                         )
 
-                        found = False
-                        for member in guild.members:
-                            if await self.bot.is_owner(member):
-                                found = True
+                    found = False
+                    for member in guild.members:
+                        if await self.bot.is_owner(member):
+                            found = True
 
+                    await guild.owner.send(
+                        "Hi! You should add your trusted server members of `"
+                        + str(guild.name)
+                        + "` to the role `gb_mod`"
+                    )
+                    await guild.owner.send(
+                        "Also, if you dislike my random meows, then you, or someone else with `gb_mod` role can run `-togglemeow` so that I'll leave the guild alone."
+                    )
+                    if not found:
                         await guild.owner.send(
-                            "Hi! You should add your trusted server members of `"
-                            + str(guild.name)
-                            + "` to the role `gb_mod`"
+                            "You should also invite the bot's owner to this guild ;)"
                         )
-                        await guild.owner.send(
-                            "Also, if you dislike my random meows, then you, or someone else with `gb_mod` role can run `-disablemeow` so that I'll leave the guild alone."
-                        )
-                        if not found:
-                            await guild.owner.send(
-                                "You should also invite the bot's owner to this guild ;)"
-                            )
-                        with open(".setupdone_" + str(message.guild.id), "w") as f:
-                            f.write("THIS IS THE NEWER VERSION OF THE LOCKFILE")
+                    with open(".setupdone_" + str(message.guild.id), "w") as f:
+                        f.write("THIS IS THE NEWER VERSION OF THE LOCKFILE")
 
     def checkmod(self, member):
         for role in member.roles:
@@ -60,13 +61,19 @@ class Admin(commands.Cog):
         return False
 
     @commands.command()
-    async def disablemeow(self, ctx):
+    async def togglemeow(self, ctx):
         if self.checkmod(ctx.message.author):
-            with open(".nomeow_" + str(message.guild.id), "w") as f:
-                f.write("No more meowing :(")
-            await ctx.send("Disabled meowing in this guild.", reference=ctx.message)
+            if not os.path.exists(".nomeow_" + str(ctx.message.guild.id)):
+                with open(".nomeow_" + str(ctx.message.guild.id), "w") as f:
+                    f.write("No more meowing :(")
+                await ctx.send("Disabled meowing in this guild.", reference=ctx.message)
+            else:
+                os.system("rm .nomeow_" + str(ctx.message.guild.id))
+                await ctx.send(
+                    "Re-enabled meowing in this guild", reference=ctx.message
+                )
         else:
-            await ctx.send("You don't have perms in this guild.")
+            await ctx.send("You don't have perms in this guild.", reference=ctx.message)
 
 
 def setup(bot):
