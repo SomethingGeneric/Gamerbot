640c01f509ae305b733d00aca17d3add1b835b23
Matt C <matthew.compton@mymail.champlain.edu>
2022-11-21 20:33:21 -0500

    Implements #28

--- bot/cogs/shell.py
+++ bot/cogs/shell.py
@@ -7,6 +7,28 @@ class Shell(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    async def reset_bash(self, ctx, confirm="n"):
+        """Reset my bash access"""
+        try:
+            if confirm == "y":
+                un = ctx.message.author.name.lower()
+                un = un.replace("-", "").replace("_", "")
+                if un[0] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                    un = un[1:]
+                await ctx.send("Resetting your user.", reference=ctx.message)
+                await run_command_shell(
+                    f"ssh punchingbag 'userdel {un} && rm -rf /home/{un}'"
+                )
+                await ctx.send("Done.", reference=ctx.message)
+            else:
+                await ctx.send(
+                    "Just to confirm, you want to nuke your bash user. If so, re-run with `-reset_bash y`",
+                    reference=ctx.message,
+                )
+        except Exception as e:
+            await ctx.send(f"Error: ```{str(e)}```", reference=ctx.message)
+
+    @commands.command()
     async def bash(self, ctx, *, cmd):
         """Run a command"""
         try:
